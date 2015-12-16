fe4e47a54e245c68b48fb632957a140f810f4178
Alexey Bezugly <alexey8bezugly@gmail.com>
2015-12-16 18:27:34 +0100

    bugfix

--- Gta5EyeTrackingModUpdater/SettingsStorage.cs
+++ Gta5EyeTrackingModUpdater/SettingsStorage.cs
@@ -14,7 +14,7 @@ namespace Gta5EyeTrackingModUpdater
 			{
 				var folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), Util.SettingsPath);
 				var filePath = Path.Combine(folderPath, SettingsFileName);
-				if (!File.Exists(SettingsFileName)) return result;
+				if (!File.Exists(filePath)) return result;
 
 				System.Xml.Serialization.XmlSerializer reader = new System.Xml.Serialization.XmlSerializer(typeof(Settings));
 				var file = new StreamReader(filePath);
