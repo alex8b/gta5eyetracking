72a77a8e334f8fdce5675c8527faa0c9e4635d9c
Alexey Bezugly (aby) <alexey.bezugly@tobii.com>
2016-02-16 12:28:23 +0100

    fix crash on slow internet connection

--- Gta5EyeTracking/GoogleAnalyticsApi.cs
+++ Gta5EyeTracking/GoogleAnalyticsApi.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace Gta5EyeTracking
@@ -38,68 +39,70 @@ namespace Gta5EyeTracking
 		private void Track(HitType type, string category, string action, string label,
 			int? value = null)
 		{
-			try
-			{
-				if (string.IsNullOrEmpty(category)) return;
-				if (string.IsNullOrEmpty(action)) return;
-
-			var request = (HttpWebRequest) WebRequest.Create("http://www.google-analytics.com/collect");
-			request.Method = "POST";
-			request.KeepAlive = false;
-
-			// the request body we want to send
-			var postData = new Dictionary<string, string>
-			{
-				{"v", "1"},
-				{"tid", _trackingId},
-				{"cid", _userGuid},
-				{"uid", _userGuid},
-				{"t", type.ToString()},
-				{"ec", category},
-				{"ea", action},
-				{"an", _applicationName},
-				{"aid", _applicationId},
-				{"av", _applicationVersion},
-			};
-			if (!string.IsNullOrEmpty(label))
-			{
-				postData.Add("el", label);
-			}
-			if (value.HasValue)
-			{
-				postData.Add("ev", value.ToString());
-			}
-
-			var postDataString = postData
-				.Aggregate("", (data, next) => string.Format("{0}&{1}={2}", data, next.Key,
-					HttpUtility.UrlEncode(next.Value)))
-				.TrimEnd('&');
-
-			// set the Content-Length header to the correct value
-			request.ContentLength = Encoding.UTF8.GetByteCount(postDataString);
-
-			// write the request body to the request
-			using (var writer = new StreamWriter(request.GetRequestStream()))
-			{
-				writer.Write(postDataString);
-			}
-
-
-				using (var webResponse = (HttpWebResponse) request.GetResponse())
+			Task.Run(() => 
+			{ 
+				try
 				{
-					if (webResponse.StatusCode != HttpStatusCode.OK)
+					if (string.IsNullOrEmpty(category)) return;
+					if (string.IsNullOrEmpty(action)) return;
+
+					var request = (HttpWebRequest) WebRequest.Create("http://www.google-analytics.com/collect");
+					request.Method = "POST";
+					request.KeepAlive = false;
+
+					// the request body we want to send
+					var postData = new Dictionary<string, string>
 					{
-						Util.Log("Google Analytics tracking did not return OK 200");
+						{"v", "1"},
+						{"tid", _trackingId},
+						{"cid", _userGuid},
+						{"uid", _userGuid},
+						{"t", type.ToString()},
+						{"ec", category},
+						{"ea", action},
+						{"an", _applicationName},
+						{"aid", _applicationId},
+						{"av", _applicationVersion},
+					};
+					if (!string.IsNullOrEmpty(label))
+					{
+						postData.Add("el", label);
 					}
-					webResponse.Close();
-                } 
-				
-			}
-			catch (Exception e)
-			{
-				Util.Log(e.Message);
-			}
-			
+					if (value.HasValue)
+					{
+						postData.Add("ev", value.ToString());
+					}
+
+					var postDataString = postData
+						.Aggregate("", (data, next) => string.Format("{0}&{1}={2}", data, next.Key,
+							HttpUtility.UrlEncode(next.Value)))
+						.TrimEnd('&');
+
+					// set the Content-Length header to the correct value
+					request.ContentLength = Encoding.UTF8.GetByteCount(postDataString);
+
+					// write the request body to the request
+					using (var writer = new StreamWriter(request.GetRequestStream()))
+					{
+						writer.Write(postDataString);
+					}
+
+
+					using (var webResponse = (HttpWebResponse) request.GetResponse())
+					{
+						if (webResponse.StatusCode != HttpStatusCode.OK)
+						{
+							Util.Log("Google Analytics tracking did not return OK 200");
+						}
+						webResponse.Close();
+					}
+
+				}
+				catch (Exception e)
+				{
+					Util.Log(e.Message);
+				}
+			});
 		}
 
 		private enum HitType
